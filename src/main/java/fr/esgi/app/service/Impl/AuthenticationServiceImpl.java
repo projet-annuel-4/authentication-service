3501b31dae0cf76cce8854f871a8ce312a57e16f
Haithem LIMAM <heithem.limame@gmail.com>
2022-07-20 20:12:30 +0200

    Delete captcha verification

--- src/main/java/fr/esgi/app/service/Impl/AuthenticationServiceImpl.java
+++ src/main/java/fr/esgi/app/service/Impl/AuthenticationServiceImpl.java
@@ -73,8 +73,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public String registerUser(User user, String captcha, String password2) {
+        /*
         String url = String.format(captchaUrl, secret, captcha);
         restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponse.class);
+        */
 
         if (user.getPassword() != null && !user.getPassword().equals(password2)) {
             throw new PasswordException("Passwords do not match.");
