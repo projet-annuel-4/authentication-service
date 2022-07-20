9304c2206e12eb7a489d55efb6287aa8f9c04add
Haithem LIMAM <heithem.limame@gmail.com>
2022-07-20 19:30:29 +0200

    Update api authorization

--- src/main/java/fr/esgi/app/configuration/WebSecurityConfiguration.java
+++ src/main/java/fr/esgi/app/configuration/WebSecurityConfiguration.java
@@ -44,7 +44,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/**/api/v1/auth/**",
                         "/**/api/v1/auth/login",
-                        "/**/api/v1/registration",
+                        "/**/api/v1/auth/registration",
                         "/websocket", "/websocket/**",
                         "/img/**",
                         "/static/**").permitAll()
